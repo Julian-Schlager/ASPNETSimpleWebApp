992594f8b5a356c19258530cc0fc5f4a19c02b98
Julian Schlager <Julian.SCHLAGER@htlvb.at>
2023-03-16 10:30:53 +0100

    Chnages Test again

--- ASPNETWebAPISample.Test/UnitTest1.cs
+++ ASPNETWebAPISample.Test/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace ASPNETWebAPISample.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(1, 0);
+            Assert.AreEqual(1, 1);
         }
     }
 }
